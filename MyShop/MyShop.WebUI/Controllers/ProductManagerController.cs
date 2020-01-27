001a45f14bbe9b092d865ec81620a8f2e0b298dd
BicksonNing <48975709+BicksonNing@users.noreply.github.com>
2020-01-27 08:23:01 +0530

    Implemented generics repository

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -10,14 +10,13 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        InMemoryRepository<Product> context;
+       InMemoryRepository<ProductCategory> productCategories;
 
         public ProductManagerController() {  
 
-        context = new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+        context = new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -62,6 +61,7 @@ namespace MyShop.WebUI.Controllers
                 ProductManagerViewModel viewModel = new ProductManagerViewModel();
                 viewModel.Product = product;
                 viewModel.ProductCategories = productCategories.Collection();
+               
                 return View(viewModel);
             }
         }
